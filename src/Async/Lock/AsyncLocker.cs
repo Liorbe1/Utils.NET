043c8ad2a3c7483fce33d04092c715a105b363f9
Yishai Wiesner <wyishai@gmail.com>
2019-01-21 14:05:20 +0200

    Split the Lock to Lock and LockAsync

--- src/Async/Lock/AsyncLocker.cs
+++ src/Async/Lock/AsyncLocker.cs
@@ -4,57 +4,62 @@ using System.Threading.Tasks;
 
 namespace CS.Utils.Async.Lock
 {
-    public class AsyncLocker : IDisposable
-    {
-        private readonly SemaphoreSlim semaphore;
+	public class AsyncLocker : IDisposable
+	{
+		private readonly SemaphoreSlim semaphore;
 
-        public AsyncLocker(int concurrentTasks = 1)
-        {
-            semaphore = new SemaphoreSlim(concurrentTasks, concurrentTasks);
-        }
+		public AsyncLocker(int concurrentTasks = 1)
+		{
+			semaphore = new SemaphoreSlim(concurrentTasks, concurrentTasks);
+		}
 
-        public async Task<LockReleaser> Lock()
-        {
-            await semaphore.WaitAsync();
-            return new LockReleaser(this);
-        }
+		public LockReleaser Lock()
+		{
+			semaphore.Wait();
+			return new LockReleaser(this);
+		}
+		public async Task<LockReleaser> LockAsync()
+		{
+			await semaphore.WaitAsync();
+			return new LockReleaser(this);
+		}
 
-        public bool Release()
-        {
-            try
-            {
-                semaphore.Release();
-                return true;
-            }
-            catch (SemaphoreFullException)
-            {
-                return false;
-            }
-        }
+		public bool Release()
+		{
+			try
+			{
+				semaphore.Release();
+				return true;
+			}
+			catch (SemaphoreFullException)
+			{
+				return false;
+			}
+		}
 
-        public void Dispose()
-        {
-            semaphore.Dispose();
-        }
-    }
+		public void Dispose()
+		{
+			semaphore.Dispose();
+		}
+	}
 
-    public class LockReleaser : IDisposable
-    {
-        private bool isDisposed = false;
-        private readonly AsyncLocker locker;
+	public class LockReleaser : IDisposable
+	{
+		private bool isDisposed = false;
+		private readonly AsyncLocker locker;
 
-        public LockReleaser(AsyncLocker locker)
-        {
-            this.locker = locker;
-        }
+		public LockReleaser(AsyncLocker locker)
+		{
+			this.locker = locker;
+		}
 
-        public void Dispose()
-        {
-            if (!isDisposed)
-            {
-                isDisposed = true;
-                locker.Release();
-            }
-        }
-    }
+		public void Dispose()
+		{
+			if (!isDisposed)
+			{
+				isDisposed = true;
+				locker.Release();
+			}
+		}
+	}
 }
