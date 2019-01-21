acf2a9a5ed02b8565a0381dbb7ff77a3bd259791
Yishai Wiesner <wyishai@gmail.com>
2019-01-21 14:10:13 +0200

    Add option to use CancellationToken and Timeout

--- src/Async/Lock/AsyncLocker.cs
+++ src/Async/Lock/AsyncLocker.cs
@@ -18,10 +18,64 @@ namespace CS.Utils.Async.Lock
 			semaphore.Wait();
 			return new LockReleaser(this);
 		}
+		public LockReleaser Lock(TimeSpan timeout)
+		{
+			if (semaphore.Wait(timeout))
+			{
+				return new LockReleaser(this);
+			}
+			else
+			{
+				throw new TimeoutException("Timeout while trying to acquire lock");
+			}
+		}
+		public LockReleaser Lock(CancellationToken cancellationToken)
+		{
+			semaphore.Wait(cancellationToken);
+			return new LockReleaser(this);
+		}
+		public LockReleaser Lock(TimeSpan timeout, CancellationToken cancellationToken)
+		{
+			if (semaphore.Wait(timeout, cancellationToken))
+			{
+				return new LockReleaser(this);
+			}
+			else
+			{
+				throw new TimeoutException("Timeout while trying to acquire lock");
+			}
+		}
 		public async Task<LockReleaser> LockAsync()
 		{
 			await semaphore.WaitAsync();
 			return new LockReleaser(this);
+		}
+		public async Task<LockReleaser> LockAsync(TimeSpan timeout)
+		{
+			if (await semaphore.WaitAsync(timeout))
+			{
+				return new LockReleaser(this);
+			}
+			else
+			{
+				throw new TimeoutException("Timeout while trying to acquire lock");
+			}
+		}
+		public async Task<LockReleaser> LockAsync(CancellationToken cancellationToken)
+		{
+			await semaphore.WaitAsync(cancellationToken);
+			return new LockReleaser(this);
+		}
+		public async Task<LockReleaser> LockAsync(TimeSpan timeout, CancellationToken cancellationToken)
+		{
+			if (await semaphore.WaitAsync(timeout, cancellationToken))
+			{
+				return new LockReleaser(this);
+			}
+			else
+			{
+				throw new TimeoutException("Timeout while trying to acquire lock");
+			}
 		}
 
 		public bool Release()
