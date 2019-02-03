5ef1f762dd4ec472d1d4065705d579d5d30ca15a
Yishai Wiesner <wyishai@gmail.com>
2019-02-03 12:19:40 +0200

    Add HashStream

---
+++ src/Crypto/HashResult.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+
+namespace CS.Utils.Crypto
+{
+	public struct HashResult : IEquatable<HashResult>, IComparable<HashResult>, IComparable
+	{
+		#region Operators
+		public static bool operator ==(HashResult a, HashResult b)
+		{
+			return a.Equals(b);
+		}
+		public static bool operator !=(HashResult a, HashResult b)
+		{
+			return !a.Equals(b);
+		}
+		public static explicit operator byte[] (HashResult hashResult)
+		{
+			return hashResult.HashBytes;
+		}
+		public static explicit operator string(HashResult hashResult)
+		{
+			return hashResult.HashString;
+		}
+		public static implicit operator HashResult(byte[] hashBytes)
+		{
+			return new HashResult(hashBytes);
+		}
+		#endregion
+
+
+		private string _hashString;
+
+		public byte[] HashBytes { get; }
+		public string HashString
+		{
+			get
+			{
+				if (_hashString != null)
+				{
+					return _hashString;
+				}
+
+				return _hashString = string.Concat(HashBytes.Select(b => b.ToString("X2")));
+			}
+		}
+
+		public HashResult(byte[] hashBytes) : this()
+		{
+			HashBytes = hashBytes;
+		}
+
+		public override string ToString()
+		{
+			return HashString;
+		}
+
+		#region Operators Methods
+		public bool Equals(HashResult other)
+		{
+			return HashBytes.SequenceEqual(other.HashBytes);
+		}
+		public override bool Equals(object obj)
+		{
+			return obj is HashResult ? Equals((HashResult)obj) : false;
+		}
+		public int CompareTo(HashResult other)
+		{
+			if (HashBytes.Length != other.HashBytes.Length)
+			{
+				return HashBytes.Length.CompareTo(other.HashBytes.Length);
+			}
+
+			for (int i = 0; i < HashBytes.Length; i++)
+			{
+				if (HashBytes[i] != other.HashBytes[i])
+				{
+					return HashBytes[i].CompareTo(other.HashBytes[i]);
+				}
+			}
+
+			return 0;
+		}
+		public int CompareTo(object obj)
+		{
+			return obj is HashResult ? CompareTo((HashResult)obj) : 1;
+		}
+
+		public override int GetHashCode()
+		{
+			return 175587528 + HashBytes.GetHashCode();
+		}
+		#endregion
+	}
+}
---
+++ src/Crypto/Streams/MultipleHashStream.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CS.Utils.Crypto.Streams
+{
+	public class MultipleHashStream : Stream
+	{
+		public override bool CanRead => baseStream.CanRead;
+		public override bool CanSeek => false;
+		public override bool CanWrite => baseStream.CanWrite;
+		public override long Length => baseStream.Length;
+		public override long Position { get => baseStream.Position; set => baseStream.Position = value; }
+		public bool HasFlushedFinalBlock
+		{
+			get
+			{
+				return readHashCalculators.All(hashCalculator => hashCalculator.HasFlushedFinalBlock) &&
+					writeHashCalculators.All(hashCalculator => hashCalculator.HasFlushedFinalBlock);
+			}
+		}
+
+		private readonly bool leaveOpen;
+		private readonly Stream baseStream;
+		private readonly List<HashStream> readHashCalculators = new List<HashStream>();
+		private readonly List<HashStream> writeHashCalculators = new List<HashStream>();
+
+		public MultipleHashStream(Stream baseStream, IEnumerable<HashAlgorithmName> hashAlgorithms, bool leaveOpen = false)
+		{
+			this.baseStream = baseStream;
+			this.leaveOpen = leaveOpen;
+
+			foreach (HashAlgorithmName hashAlgorithmName in hashAlgorithms)
+			{
+				readHashCalculators.Add(new HashStream(hashAlgorithmName));
+				writeHashCalculators.Add(new HashStream(hashAlgorithmName));
+			}
+		}
+
+		public override void Flush() => baseStream.Flush();
+		public override void SetLength(long value) => baseStream.SetLength(value);
+		public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+		public void FlushFinalBlock()
+		{
+			if (readHashCalculators != null)
+			{
+				foreach (HashStream hashCalculator in readHashCalculators)
+				{
+					hashCalculator.FlushFinalBlock();
+				}
+			}
+			if (writeHashCalculators != null)
+			{
+				foreach (HashStream hashCalculator in writeHashCalculators)
+				{
+					hashCalculator.FlushFinalBlock();
+				}
+			}
+		}
+
+		public override Task FlushAsync(CancellationToken cancellationToken)
+		{
+			return baseStream.FlushAsync(cancellationToken);
+		}
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			return ReadAsync(buffer, offset, count).Result;
+		}
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			WriteAsync(buffer, offset, count).Wait();
+		}
+		public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+		{
+			int readedBytes = await baseStream.ReadAsync(buffer, offset, count, cancellationToken);
+
+			if (readHashCalculators != null && readedBytes > 0)
+			{
+				await Task.WhenAll(readHashCalculators.Select(hashCalculator => hashCalculator.WriteAsync(buffer, offset, readedBytes)).ToList());
+			}
+
+			return readedBytes;
+		}
+		public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+		{
+			Task baseStreamWriteTask = baseStream.WriteAsync(buffer, offset, count, cancellationToken);
+			await baseStreamWriteTask;
+
+			if (writeHashCalculators != null && !baseStreamWriteTask.IsCanceled)
+			{
+				await Task.WhenAll(writeHashCalculators.Select(hashCalculator => hashCalculator.WriteAsync(buffer, offset, count)).ToList());
+			}
+		}
+	}
+}
