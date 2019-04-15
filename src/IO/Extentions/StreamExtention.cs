89401369042df5f0dc2127dc8d7559d844f43a31
Yishai Wiesner <wyishai@gmail.com>
2019-04-15 17:53:38 +0300

    no message

--- src/IO/Extentions/StreamExtention.cs
+++ src/IO/Extentions/StreamExtention.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using System.Threading.Tasks;
+using CS.Utils.IO.Streams;
 using Types.BytesSize;
 
 namespace CS.Utils.IO.Extentions
