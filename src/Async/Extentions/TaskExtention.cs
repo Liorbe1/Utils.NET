547decc5d6870cfb1144cfdd3074632bc1bcb0d6
Yishai Wiesner <wyishai@gmail.com>
2019-04-18 16:37:44 +0300

    Add SetTimeout operation

---
+++ src/Async/Extentions/TaskExtention.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CS.Utils.Async.Extentions
+{
+	public static class TaskExtention
+	{
+		public static async Task SetTimeout(this Task task, TimeSpan timeout)
+		{
+			using (CancellationTokenSource timeoutCancellationTokenSource = new CancellationTokenSource())
+			{
+				Task delayTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+				await Task.WhenAny(task, delayTask);
+
+				if (!task.IsCompleted)
+				{
+					throw new TimeoutException("Operation timed out");
+				}
+				else
+				{
+					timeoutCancellationTokenSource.Cancel();
+				}
+			}
+		}
+		public static async Task<TResult> SetTimeout<TResult>(this Task<TResult> task, TimeSpan timeout)
+		{
+			await (task as Task).SetTimeout(timeout);
+			return await task;
+		}
+	}
+}
