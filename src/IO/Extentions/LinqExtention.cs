a4e2731df8afbe042a511c716f7ecef9b9be53ac
Yishai Wiesner <wyishai@gmail.com>
2019-06-20 15:26:45 +0300

    Add repeat extention

---
+++ src/IO/Extentions/LinqExtention.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS.Utils.IO.Extentions
+{
+	public static class LinqExtention
+	{
+		public static IEnumerable<T> Repeat<T>(this T item, int times)
+		{
+			for (int i = 0; i < times; i++)
+			{
+				yield return item;
+			}
+		}
+	}
+}
